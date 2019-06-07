4ceae60e563714b4bd48a1765588f85e6a54139b
prasanthipenmadu <prasanthipl@gmail.com>
2019-06-07 10:03:00 -0400

    All tests Passed

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -1,15 +1,24 @@
 package rocks.zipcode.io;
 
+import java.util.Arrays;
 import java.util.Comparator;
 
 /**
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    Item items[];
     public ItemSorter(Item[] items) {
+        this.items=items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
-        return null;
+        Comparator<Item> idComparator=Comparator.comparing(Item::getId);
+      // Comparator<Item> nameComparator=Comparator.comparing(Item::getName);
+       // Comparator<Item> priceComparator=Comparator.comparing(Item::getPrice);
+       // Arrays.sort(items,priceComparator);
+        Arrays.sort(items,idComparator);
+        //Arrays.sort(items,nameComparator);
+        return items;
     }
 }
--- src/main/java/rocks/zipcode/io/comparators/NameComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/NameComparator.java
@@ -1,7 +1,21 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class NameComparator {
+public class NameComparator implements Comparator<Item>{
+
+    public int compare(Item o1, Item o2) {
+        if(o1.getName().compareTo(o2.getName())==0)
+            return 0;
+       else if(o1.getName().compareTo(o2.getName())>0)
+            return 1;
+       else
+           return -1;
+    }
 }
+
